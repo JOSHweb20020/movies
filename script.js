6f73206ded7c167df9a27bcfd8cde70b38e7184a
Oyebade Seyi <oyebadeseyi7@gmail.com>
2022-08-29 08:09:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const navBar = document.querySelector('.navbar')
+const sideBar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+const element = document.querySelector('.element')
+
+
+
+
+
+
---
+++ tutorial sidebar/main.js
@@ -0,0 +1,5 @@
+const sidebar = document.querySelector('.sidebar')
+
+function click(){
+    sidebar.classList.add('active');
+}
